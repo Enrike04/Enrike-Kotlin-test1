261d210f0039e4f4ee18ba4c69ec5fd52ebc899b
Kirill Bochkarev <kirill.bochkarev@jetbrains.com>
2023-11-02 20:08:11 +0100

    fix main

--- oop/src/main/kotlin/org/jetbrains/edu/Main.kt
+++ oop/src/main/kotlin/org/jetbrains/edu/Main.kt
@@ -1,12 +1,14 @@
 package org.jetbrains.edu
 
 fun main() {
-    val trie = TrieImpl()
-    trie.add("her")
-    trie.add("hero")
-    trie.add("hers")
-    trie.add("hereafter")
-    trie.printAllPaths()
-    trie.remove("hereafter")
-    trie.printAllPaths()
+    val trie: Trie = TrieImpl()
+    assert(trie.add("her"))
+    assert(trie.add("hero"))
+    assert(trie.add("hers"))
+    assert(trie.add("hereafter"))
+    assert(!trie.add("her"))
+    assert(trie.howManyStartsWithPrefix("her") == 4)
+    assert(trie.remove("her"))
+    assert(!trie.remove("her"))
+    assert(trie.howManyStartsWithPrefix("her") == 3)
 }
