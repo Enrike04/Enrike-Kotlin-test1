e4e1344e1642b4de26170163494ce057682fe1b9
Enrike Valentinov <evalentinov@constructor.university>
2024-04-11 22:26:13 +0200

    First Try

--- basic/src/main/kotlin/org/jetbrains/edu/ByteArrayZip.kt
+++ basic/src/main/kotlin/org/jetbrains/edu/ByteArrayZip.kt
@@ -1,9 +1,40 @@
 package org.jetbrains.edu
 
 fun ByteArray.zip(): ByteArray {
-    TODO()
+    if (this.isEmpty()) return ByteArray(0)
+
+    val result = mutableListOf<Byte>()
+    var currentByte = this[0]
+    var count = 1
+
+    for (i in 1 until this.size) {
+        if (this[i] == currentByte && count < 255) {
+            count++
+        } else {
+            result.add(count.toByte())
+            result.add(currentByte)
+            currentByte = this[i]
+            count = 1
+        }
+    }
+
+    result.add(count.toByte())
+    result.add(currentByte)
+
+    return result.toByteArray()
 }
 
 fun ByteArray.unZip(): ByteArray {
-    TODO()
+    if (this.isEmpty()) return ByteArray(0)
+
+    val result = mutableListOf<Byte>()
+
+    for (i in indices step (2)) {
+        val count = this[i].toInt() and 0xFF
+        val byte = this[i + 1]
+        repeat(count) {
+            result.add(byte)
+        }
+    }
+    return result.toByteArray()
 }
--- oop/src/main/kotlin/org/jetbrains/edu/TrieImpl.kt
+++ oop/src/main/kotlin/org/jetbrains/edu/TrieImpl.kt
@@ -1,23 +1,72 @@
 package org.jetbrains.edu
 
+data class TrieNode(var wordsCount: Int = 0) {
+    val children = HashMap<Char, TrieNode>()
+    var isEndOfWord = false
+}
+
 class TrieImpl : Trie {
+    private val root = TrieNode()
+
     override fun add(element: String): Boolean {
-        TODO()
+        var current = root
+        if (contains(element)) return false
+
+        for (char in element) {
+            if (!current.children.containsKey(char)) {
+                current.children[char] = TrieNode()
+            }
+            current.wordsCount++
+            current = current.children[char] ?: return false
+        }
+        current.isEndOfWord = true
+        current.wordsCount++
+
+        return true
     }
 
     override fun contains(element: String): Boolean {
-        TODO()
+        var current = root
+        element.forEach { char ->
+            if (!current.children.containsKey(char)) {
+                return false
+            }
+            current = current.children[char] ?: return false
+        }
+        return current.isEndOfWord
     }
 
     override fun remove(element: String): Boolean {
-        TODO()
+        var current = root
+        when (contains(element)) {
+            true -> {
+                element.forEach { char ->
+                    if (!current.children.containsKey(char)) {
+                        return false
+                    }
+                    current.wordsCount--
+                    current = current.children[char] ?: return false
+                }
+                current.isEndOfWord = false
+                current.wordsCount--
+
+                return true
+            }
+
+            false -> return false
+        }
     }
 
-    override fun size(): Int {
-        TODO()
-    }
+    override fun size(): Int = root.wordsCount
 
     override fun howManyStartsWithPrefix(prefix: String): Int {
-        TODO()
+        var current = root
+        for (char in prefix) {
+            if (!current.children.containsKey(char)) {
+                return 0
+            }
+            current = current.children[char] ?: return 0
+        }
+        return current.wordsCount
     }
 }
